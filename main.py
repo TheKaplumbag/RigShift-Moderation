4a203453de314826d26ba9caa9358f6522e062ca
TheKaplumbag <scpmtf4@gmail.com>
2026-08-09 20:53:48 +0300

    Quick commit: Update main.py

--- main.py
+++ main.py
@@ -54,9 +54,9 @@ class Bot(commands.Bot):
 
   
   
-  @tasks.loop(seconds=15)
+  @tasks.loop(seconds=15.0)
   async def change_status(self):
-    new_status : str = random.choice(Status_list)
+    new_status : str = random.choice(*Status_list)
     await self.change_presence(
       status=discord.Status.online,
       activity=discord.CustomActivity(name=new_status),
@@ -68,6 +68,7 @@ class Bot(commands.Bot):
 
     await self.load_extension("Cogs.StaffCog")
     await self.load_extension("Cogs.OffenderCog")
+    await self.load_extension("Cogs.AdminCog")
 
     try:
       if DEV_GUILD and DEV_GUILD.isdigit():
